822bcf838f43f33757801ac576155dc3b7492269
Adam Filewicz <adam.filewicz.stud@pw.edu.pl>
2024-11-22 21:17:52 +0100

    sh for 1.2

--- zad12/python_client/run_client.sh
+++ zad12/python_client/run_client.sh
@@ -1,2 +1,1 @@
-#bin/bash
-docker run --name z16_lab11_python_client --network z16_network z16_lab11_python_client
+docker run --name z16_lab12_python_client --network z16_network z16_lab12_python_client
--- zad12/python_server/run_server.sh
+++ zad12/python_server/run_server.sh
@@ -1,2 +1,1 @@
-#bin/bash
-docker run --name z16_lab11_python_server --network z16_network z16_lab11_python_server
+docker run --name z16_lab12_python_server --network z16_network z16_lab12_python_server
